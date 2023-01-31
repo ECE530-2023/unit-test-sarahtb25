0f8d3ea62bcb8cb670ad67bfe1cd36a35a7985cf
sarahtb25 <sbonna@bu.edu>
2023-01-31 12:11:15 -0500

    Changed the way errors are caught and the structure of test_matrix_multiplier.py

--- matrix_multiplier.py
+++ matrix_multiplier.py
@@ -7,26 +7,23 @@ def matrix_multiplier(first_matrix, second_matrix):
     # print()
     # print(second_matrix)
 
-    result = check_matrices(first_matrix, second_matrix)
+    check_matrices(first_matrix, second_matrix)
 
     # Matrix multiplication done here
-    if (result == "OK"):
-        number_of_rows_of_first_matrix = len(first_matrix)
-        number_of_columns_of_second_matrix = len(second_matrix[0])
-        answer = [[0 for i in range(number_of_columns_of_second_matrix)]
-                  for j in range(number_of_rows_of_first_matrix)]
+    number_of_rows_of_first_matrix = len(first_matrix)
+    number_of_columns_of_second_matrix = len(second_matrix[0])
+    answer = [[0 for i in range(number_of_columns_of_second_matrix)]
+              for j in range(number_of_rows_of_first_matrix)]
 
-        # iterate through rows of first_matrix
-        for i in range(len(first_matrix)):
-            # iterate through columns of second_matrix
-            for j in range(len(second_matrix[0])):
-                # iterate through rows of second_matrix
-                for k in range(len(second_matrix)):
-                    answer[i][j] += first_matrix[i][k] * second_matrix[k][j]
+    # iterate through rows of first_matrix
+    for i in range(len(first_matrix)):
+        # iterate through columns of second_matrix
+        for j in range(len(second_matrix[0])):
+            # iterate through rows of second_matrix
+            for k in range(len(second_matrix)):
+                answer[i][j] += first_matrix[i][k] * second_matrix[k][j]
 
-        return answer
-
-    return result
+    return answer
 
 
 def check_matrices(first_matrix, second_matrix):
@@ -35,26 +32,11 @@ def check_matrices(first_matrix, second_matrix):
     If valid, calls function to check if there are any non-integer or non-float values.
     '''
     # Check dimensions of matrices
-    check_dimensions_of_matrices = check_dimensions(
-        first_matrix, second_matrix)
-
-    if (check_dimensions_of_matrices != "OK"):
-        return check_dimensions_of_matrices
+    check_dimensions(first_matrix, second_matrix)
 
     # Check if there are any non-integer or non-float values
-    check_values_first_matrix = check_matrix_for_non_integer_or_float_values(
-        first_matrix)
-
-    if (check_values_first_matrix != "OK"):
-        return check_values_first_matrix
-
-    check_values_second_matrix = check_matrix_for_non_integer_or_float_values(
-        second_matrix)
-
-    if (check_values_second_matrix != "OK"):
-        return check_values_second_matrix
-
-    return "OK"
+    check_matrix_for_non_integer_or_float_values(first_matrix)
+    check_matrix_for_non_integer_or_float_values(second_matrix)
 
 
 def check_dimensions(first_matrix, second_matrix):
@@ -63,21 +45,15 @@ def check_dimensions(first_matrix, second_matrix):
     If all is fine, checks if the number of columns of the first matrix == number of rows of the second matrix
     '''
     # Check if there are any empty rows in the matrices
-    check_empty_row = check_empty_row_in_matrix(first_matrix)
-    if check_empty_row != "OK":
-        return check_empty_row
-
-    check_empty_row = check_empty_row_in_matrix(second_matrix)
-    if check_empty_row != "OK":
-        return check_empty_row
+    check_empty_row_in_matrix(first_matrix)
+    check_empty_row_in_matrix(second_matrix)
 
     # Checks if the number of columns of the first matrix == number of rows of the second matrix
     first_number_of_columns = len(first_matrix[0])
     second_number_of_rows = len(second_matrix)
     if (first_number_of_columns != second_number_of_rows):
-        return "Number of columns of first matrix does not match number of rows of second matrix!"
-
-    return "OK"
+        raise ValueError(
+            "Number of columns of first matrix does not match number of rows of second matrix!")
 
 
 def check_empty_row_in_matrix(matrix):
@@ -85,15 +61,13 @@ def check_empty_row_in_matrix(matrix):
     Checks if the matrix is empty or if it has any empty rows
     '''
     if len(matrix) == 1 and len(matrix[0]) == 0:
-        return "Empty matrix!"
+        raise ValueError("Empty matrix!")
     else:
         base_length_of_row = len(matrix[0])
 
         for row in range(len(matrix)):
             if len(matrix[row]) != base_length_of_row:
-                return "Length of rows in matrix are not equal!"
-
-    return "OK"
+                raise ValueError("Length of rows in matrix are not equal!")
 
 
 def check_matrix_for_non_integer_or_float_values(matrix):
@@ -103,6 +77,4 @@ def check_matrix_for_non_integer_or_float_values(matrix):
     for row in range(len(matrix)):
         for column in range(len(matrix[row])):
             if not (isinstance(matrix[row][column], int) or isinstance(matrix[row][column], float)):
-                return "Non-integer value given!"
-
-    return "OK"
+                raise ValueError("Non-integer value given!")
--- test_matrix_multiplier.py
+++ test_matrix_multiplier.py
@@ -1,31 +1,90 @@
 import matrix_multiplier
-
-known_cases = (([['a', 1], [2, 3]], [[1, 2], [3, 4]], "Non-integer value given!"),
-               ([[1, 2]], [[
-                1, 2, 3]], "Number of columns of first matrix does not match number of rows of second matrix!"),
-               ([[]], [[1, 2]], "Empty matrix!"),
-               ([[1, 2]], [
-                []], "Empty matrix!"),
-               ([[]], [[1]], "Empty matrix!"),
-               ([[1, 2], [], [2, 3]], [[1, 2], [3, 5]],
-                "Length of rows in matrix are not equal!"),
-               ([[1, 2], [[1]], [2, 3]], [[1, 2], [3, 5]],
-                "Length of rows in matrix are not equal!"),
-               ([[], [[1]], [2, 3]], [[1, 2], [3, 5]],
-                "Length of rows in matrix are not equal!"),
-               ([[1, 2], [3, 4]], [[5, 6, 7], [8, 9, 10]],
-                [[21, 24, 27], [47, 54, 61]]),
-               ([[1, 0.5]], [[2, 4], [6, 8]], [[5, 8]]))
+import pytest
 
 
-def test_matrix_multiplier():
-    for first_matrix, second_matrix, expected_result in known_cases:
-        result = matrix_multiplier.matrix_multiplier(
+def test_non_integer_value_1():
+    first_matrix = [['a', 1], [2, 3]]
+    second_matrix = [[1, 2], [3, 4]]
+
+    with pytest.raises(ValueError, match="Non-integer value given!"):
+        matrix_multiplier.matrix_multiplier(
             first_matrix, second_matrix)
-        # print(result)
-
-        assert expected_result == result
 
 
-if __name__ == '__main__':
-    test_matrix_multiplier()
+def test_rows_columns_mismatch_2():
+    first_matrix = [[1, 2]]
+    second_matrix = [[1, 2, 3]]
+
+    with pytest.raises(ValueError, match="Number of columns of first matrix does not match number of rows of second matrix!"):
+        matrix_multiplier.matrix_multiplier(
+            first_matrix, second_matrix)
+
+
+def test_first_matrix_empty_3():
+    first_matrix = [[]]
+    second_matrix = [[1, 2]]
+
+    with pytest.raises(ValueError, match="Empty matrix!"):
+        matrix_multiplier.matrix_multiplier(
+            first_matrix, second_matrix)
+
+
+def test_first_matrix_empty_4():
+    first_matrix = [[]]
+    second_matrix = [[1]]
+
+    with pytest.raises(ValueError, match="Empty matrix!"):
+        matrix_multiplier.matrix_multiplier(
+            first_matrix, second_matrix)
+
+
+def test_second_matrix_empty_5():
+    first_matrix = [[1, 2]]
+    second_matrix = [[]]
+
+    with pytest.raises(ValueError, match="Empty matrix!"):
+        matrix_multiplier.matrix_multiplier(
+            first_matrix, second_matrix)
+
+
+def test_lengths_of_matrices_mismatch_6():
+    first_matrix = [[1, 2], [], [2, 3]]
+    second_matrix = [[1, 2], [3, 5]]
+
+    with pytest.raises(ValueError, match="Length of rows in matrix are not equal!"):
+        matrix_multiplier.matrix_multiplier(
+            first_matrix, second_matrix)
+
+
+def test_lengths_of_matrices_mismatch_7():
+    first_matrix = [[1, 2], [[1]], [2, 3]]
+    second_matrix = [[1, 2], [3, 5]]
+
+    with pytest.raises(ValueError, match="Length of rows in matrix are not equal!"):
+        matrix_multiplier.matrix_multiplier(
+            first_matrix, second_matrix)
+
+
+def test_lengths_of_matrices_mismatch_8():
+    first_matrix = [[], [[1]], [2, 3]]
+    second_matrix = [[1, 2], [3, 5]]
+
+    with pytest.raises(ValueError, match="Length of rows in matrix are not equal!"):
+        matrix_multiplier.matrix_multiplier(
+            first_matrix, second_matrix)
+
+
+def test_matrix_multiplication_9():
+    first_matrix = [[1, 2], [3, 4]]
+    second_matrix = [[5, 6, 7], [8, 9, 10]]
+
+    assert matrix_multiplier.matrix_multiplier(
+        first_matrix, second_matrix) == [[21, 24, 27], [47, 54, 61]]
+
+
+def test_matrix_multiplication_10():
+    first_matrix = [[1, 0.5]]
+    second_matrix = [[2, 4], [6, 8]]
+
+    assert matrix_multiplier.matrix_multiplier(
+        first_matrix, second_matrix) == [[5, 8]]
